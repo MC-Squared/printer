24f5cd4ba7c785f5e10b89522d58bad5c6e803c0
J Miles <j.milesy.nz@gmail.com>
2017-06-09 17:26:44 +1200

    Tidy up calculations

--- lib/assemblies/printer_assembly.rb
+++ lib/assemblies/printer_assembly.rb
@@ -11,114 +11,147 @@ class PrinterAssembly < SolidRuby::Assembly
   end
 
   def part(show)
-    #printable area
-    @printable_x = 550
-    @printable_y = 550
-    @printable_z = 550
+    @extrusion_w = 20
+    @extrusion_h = 40
 
+    #printable area
+    @printable_x = 500
+    @printable_y = 500
+    @printable_z = 500
+
+    #X carriage size (determines how much we need to add to printable_* to achieve it)
+    @carriage_x = 40
+    @carriage_y = 40
+    @carriage_z = 40
+
+    #Size of gantry plates
+    @plate_w = 100
+    @plate_t = 10 #thickness
+
+    #internal area, i.e. the extrusions should be *outside* this box
+    @in_x = @printable_x + @carriage_x
+    @in_y = @printable_y + @carriage_y + @plate_t + @extrusion_w
+    @in_z = @printable_z + @carriage_z
+
+    #internal frame, i.e. the box that holds the X/Y gantries
+    @if_x = @in_x + @extrusion_w * 2
+    @if_y = @in_y + @extrusion_w * 2
+
+    @out_z = @in_z + @extrusion_h * 3 #base surrounds, top surrounds, carriage
+    @out_y = @in_y + @extrusion_w * 4 #surrounds * 2, carriage * 2
 
     # printable area
-     res = cube(@printable_x, @printable_y, @printable_z)
-       .translate(x: Al2040::WIDTH*2.0, y: Al2040::WIDTH*2.0, z: Al2040::DEPTH)
+    #  res = cube(@printable_x, @printable_y, @printable_z)
+    #    .translate(x: @extrusion_w*2.0, y: @extrusion_w*2.0, z: @extrusion_h)
+    #    .color("LIGHTBLUE")
+
+     res = cube(@in_x, @in_y, @in_z)
+       .translate(x: @extrusion_w*2.0, y: @extrusion_w*2.0, z: @extrusion_h)
        .color("LIGHTBLUE")
 
+    back_pos = @in_y + @extrusion_w * 2.0
+    right_pos = @in_x + @extrusion_w * 3.0
+    top_pos = @in_z + @extrusion_h * 2.0
     #4 corner posts
-    res += Al2040.new(@printable_z + Al2040::DEPTH*3.0)
+    res += Al2040.new(@out_z)
 
-    res += Al2040.new(@printable_z + Al2040::DEPTH*3.0)
-      .translate(y: @printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH).debug
+    res += Al2040.new(@out_z)
+      .translate(y: back_pos)
 
-    res += Al2040.new(@printable_z + Al2040::DEPTH*3.0)
-      .translate(y: @printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH)
-      .translate(x: Al2040::WIDTH * 3.0 + @printable_x)
+    res += Al2040.new(@out_z)
+      .translate(y: back_pos)
+      .translate(x: right_pos)
 
-    res += Al2040.new(@printable_z + Al2040::DEPTH*3.0)
-      .translate(x: Al2040::WIDTH * 3.0 + @printable_x)
-
+    res += Al2040.new(@out_z)
+      .translate(x: right_pos)
 
 
     # 4 base sides
-    res += Al2040.new(@printable_x + Al2040::WIDTH*2.0)
+    res += Al2040.new(@if_x)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH)
-    res += Al2040.new(@printable_x + Al2040::WIDTH*2.0)
+      .translate(x: @extrusion_w)
+    res += Al2040.new(@if_x)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH, y: @printable_y + Al2040::WIDTH*3.0)
+      .translate(x: @extrusion_w, y: back_pos + @extrusion_w)
 
-    res += Al2040.new(@printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH*2.0)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(y: 90)
       .rotate(z: 90)
-      .translate(x: Al2040::WIDTH, y: Al2040::DEPTH)
+      .translate(x: @extrusion_w, y: @extrusion_h)
       #.debug
-    res += Al2040.new(@printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH*2.0)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(y: 90)
       .rotate(z: 90)
-      .translate(x: @printable_x + Al2040::WIDTH * 4.0, y: Al2040::DEPTH)
+      .translate(x: right_pos + @extrusion_w, y: @extrusion_h)
 
     # 4 top sides
-    res += Al2040.new(@printable_x + Al2040::WIDTH * 2.0)
+    res += Al2040.new(@if_x)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH, z: @printable_z + Al2040::DEPTH*2.0)
-    res += Al2040.new(@printable_x + Al2040::WIDTH * 2.0)
+      .translate(x: @extrusion_w, z: top_pos)
+    res += Al2040.new(@if_x)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH, y: @printable_y + Al2040::WIDTH*3.0, z: @printable_z + Al2040::DEPTH*2.0)
+      .translate(x: @extrusion_w, y: back_pos + @extrusion_w, z: top_pos)
 
-    res += Al2040.new(@printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH*2.0)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(y: 90)
       .rotate(z: 90)
-      .translate(x: Al2040::WIDTH, y: Al2040::DEPTH, z: @printable_z + Al2040::DEPTH*2.0)
-    res += Al2040.new(@printable_y + Al2040::WIDTH*4.0 - Al2040::DEPTH*2.0)
+      .translate(x: @extrusion_w, y: @extrusion_h, z: top_pos)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(y: 90)
       .rotate(z: 90)
-      .translate(x: @printable_x + Al2040::WIDTH * 4.0, y: Al2040::DEPTH, z: @printable_z + Al2040::DEPTH*2.0)
+      .translate(x: right_pos + @extrusion_w, y: @extrusion_h, z: top_pos)
 
+    #top_pos -= 50
     #Y Carriage
-    res += Al2040.new(@printable_y + Al2040::WIDTH*2.0)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(x: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH, y: @printable_y + Al2040::WIDTH*3.0, z: @printable_z + Al2040::DEPTH)
+      .translate(x: @extrusion_w, y: @if_y + @extrusion_w, z: top_pos - @extrusion_h)
       .debug
-    res += Al2040.new(@printable_x)
+    res += Al2040.new(@if_x - @extrusion_w * 2.0)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH*2.0, y: @printable_y + Al2040::WIDTH*2.0, z: @printable_z + Al2040::DEPTH)
+      .translate(x: @extrusion_w*2.0, y: back_pos, z: top_pos - @extrusion_h)
       #.debug
-    res += Al2040.new(@printable_y + Al2040::WIDTH*2.0)
+    res += Al2040.new(@if_y)
       .rotate(z: 90)
       .rotate(x: -90)
       .rotate(y: 90)
-      .translate(x: @printable_x + Al2040::WIDTH*3.0, y: Al2040::WIDTH, z: @printable_z + Al2040::DEPTH)
+      .translate(x: right_pos, y: @extrusion_w, z: top_pos - @extrusion_h)
       .debug
-    res += Al2040.new(@printable_x)
+    res += Al2040.new(@if_x - @extrusion_w * 2.0)
       .rotate(z: 90)
       .rotate(y: 90)
-      .translate(x: Al2040::WIDTH*2.0, y: Al2040::WIDTH, z: @printable_z + Al2040::DEPTH)
+      .translate(x: @extrusion_w * 2.0, y: @extrusion_w, z: top_pos - @extrusion_h)
       #.debug
 
     #Z Motors
     res += Nema17.new
-      .translate(x: Al2040::WIDTH, y: @printable_y + Al2040::WIDTH*3.0 - Nema17::WIDTH/2.0, z: Al2040::DEPTH)
+      .translate(x: @extrusion_w, y: @printable_y + @extrusion_w*3.0 - Nema17::WIDTH/2.0, z: @extrusion_h)
 
     #Z Rods
-    res += cylinder(d: 8, h: @printable_z + Al2040::DEPTH)
-      .translate(x: (@printable_x + Al2040::WIDTH*4.0)/2.0, y: @printable_y + Al2040::WIDTH*3.5, z: Al2040::DEPTH)
-    res += cylinder(d: 8, h: @printable_z + Al2040::DEPTH)
-      .translate(x: Al2040::WIDTH/2.0, y: (@printable_y + Al2040::WIDTH*2.0)/2.0 + Al2040::WIDTH, z: Al2040::DEPTH)
-    res += cylinder(d: 8, h: @printable_z + Al2040::DEPTH)
-      .translate(x: (@printable_x + Al2040::WIDTH*3.5), y: (@printable_y + Al2040::WIDTH*2.0)/2.0 + Al2040::WIDTH, z: Al2040::DEPTH)
+    res += cylinder(d: 8, h: @in_z + @extrusion_h)
+      .translate(x: -4, y: -4)
+      .translate(x: (@in_x + @extrusion_w*4.0)/2.0, y: @in_y + @extrusion_w*3.5, z: @extrusion_h)
+    res += cylinder(d: 8, h: @in_z + @extrusion_h)
+      .translate(x: -4, y: -4)
+      .translate(x: @extrusion_w/2.0, y: (@in_y + @extrusion_w*2.0)/2.0 + @extrusion_w, z: @extrusion_h)
+    res += cylinder(d: 8, h: @in_z + @extrusion_h)
+      .translate(x: -4, y: -4)
+      .translate(x: (@in_x + @extrusion_w*3.5), y: (@in_y + @extrusion_w*2.0)/2.0 + @extrusion_w, z: @extrusion_h)
 
     #Extruder (E3D V6)
     res += cube(41, 30, 63.5)
-      .translate(x: @printable_x/2.0, y: ((@printable_y + Al2040::DEPTH*2.0)/2.0 - Al2040::WIDTH*1.5) * 2, z: @printable_z + Al2040::DEPTH - 10)
+      .translate(x: @printable_x/2.0, y: ((@printable_y + @extrusion_h*2.0)/2.0 - @extrusion_w*1.5) * 2, z: @printable_z + @extrusion_h - 10)
       .color("BLUE")
     res
   end
--- output/PrinterAssembly_show.scad
+++ output/PrinterAssembly_show.scad
@@ -1,95 +1,98 @@
 $fn=64;
 union(){color("LIGHTBLUE"){translate(v = [40.000, 40.000, 40])
-cube(size = [550, 550, 550]);
+cube(size = [540, 570, 540]);
 }
-color("GREY"){cube(size = [20, 40, 670.000]);
+color("GREY"){cube(size = [20, 40, 660]);
 }
-#translate(v = [0, 590.000])
-color("GREY"){cube(size = [20, 40, 670.000]);
+translate(v = [0, 610.000])
+color("GREY"){cube(size = [20, 40, 660]);
 }
-translate(v = [610.000, 0])
-translate(v = [0, 590.000])
-color("GREY"){cube(size = [20, 40, 670.000]);
+translate(v = [600.000, 0])
+translate(v = [0, 610.000])
+color("GREY"){cube(size = [20, 40, 660]);
 }
-translate(v = [610.000, 0])
-color("GREY"){cube(size = [20, 40, 670.000]);
+translate(v = [600.000, 0])
+color("GREY"){cube(size = [20, 40, 660]);
 }
 translate(v = [20, 0])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 580]);
 }
-translate(v = [20, 610.000])
+translate(v = [20, 630.000])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 580]);
 }
 translate(v = [20, 40])
 rotate(a = [0, 0, 90])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550.000]);
+color("GREY"){cube(size = [20, 40, 610]);
 }
-translate(v = [630.000, 40])
+translate(v = [620.000, 40])
 rotate(a = [0, 0, 90])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550.000]);
+color("GREY"){cube(size = [20, 40, 610]);
 }
-translate(v = [20, 0, 630.000])
+translate(v = [20, 0, 620.000])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 580]);
 }
-translate(v = [20, 610.000, 630.000])
+translate(v = [20, 630.000, 620.000])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 580]);
 }
-translate(v = [20, 40, 630.000])
-rotate(a = [0, 0, 90])
-rotate(a = [0, 90, 0])
-rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550.000]);
-}
-translate(v = [630.000, 40, 630.000])
+translate(v = [20, 40, 620.000])
 rotate(a = [0, 0, 90])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550.000]);
+color("GREY"){cube(size = [20, 40, 610]);
 }
-#translate(v = [20, 610.000, 590])
+translate(v = [620.000, 40, 620.000])
+rotate(a = [0, 0, 90])
+rotate(a = [0, 90, 0])
+rotate(a = [0, 0, 90])
+color("GREY"){cube(size = [20, 40, 610]);
+}
+#translate(v = [20, 630, 580.000])
 rotate(a = [0, 90, 0])
 rotate(a = [90, 0, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 610]);
 }
-translate(v = [40.000, 590.000, 590])
+translate(v = [40.000, 610.000, 580.000])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550]);
+color("GREY"){cube(size = [20, 40, 540.000]);
 }
-#translate(v = [610.000, 20, 590])
+#translate(v = [600.000, 20, 580.000])
 rotate(a = [0, 90, 0])
 rotate(a = [-90, 0, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 590.000]);
+color("GREY"){cube(size = [20, 40, 610]);
 }
-translate(v = [40.000, 20, 590])
+translate(v = [40.000, 20, 580.000])
 rotate(a = [0, 90, 0])
 rotate(a = [0, 0, 90])
-color("GREY"){cube(size = [20, 40, 550]);
+color("GREY"){cube(size = [20, 40, 540.000]);
 }
-translate(v = [20, 588.500, 40])
+translate(v = [20, 538.500, 40])
 color("BLUE"){cube(size = [43, 43, 60]);
 }
-translate(v = [315.000, 620.000, 40])
-cylinder(h = 590, r = 4.000);
-translate(v = [10.000, 315.000, 40])
-cylinder(h = 590, r = 4.000);
-translate(v = [620.000, 315.000, 40])
-cylinder(h = 590, r = 4.000);
-color("BLUE"){translate(v = [275.000, 570.000, 580])
+translate(v = [310.000, 640.000, 40])
+translate(v = [-4, -4])
+cylinder(h = 580, r = 4.000);
+translate(v = [10.000, 325.000, 40])
+translate(v = [-4, -4])
+cylinder(h = 580, r = 4.000);
+translate(v = [610.000, 325.000, 40])
+translate(v = [-4, -4])
+cylinder(h = 580, r = 4.000);
+color("BLUE"){translate(v = [250.000, 520.000, 530])
 cube(size = [41, 30, 63.500]);
 }
 }
